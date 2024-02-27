d5438aefb628f89bc7e8fcd0d573c8bc30cf2fb1
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2024-02-27 19:10:26 +0200

    circle

--- Annotator/CircleAnnotator.cs
+++ Annotator/CircleAnnotator.cs
@@ -1,9 +1,29 @@
+using Aspose.Pdf;
+using Aspose.Pdf.Annotations;
+
 namespace aspose.pdf.annotation;
 
 public class CircleAnnotator : IAnnotator
 {
     public void Add()
     {
-        throw new NotImplementedException();
+        var dataDir = "<path-to-file>";
+        // Load the PDF file
+        Document document = new Document(System.IO.Path.Combine(dataDir, "appartments.pdf"));
+
+        // Create Cirlce Annotation
+        var circleAnnotation = new CircleAnnotation(document.Pages[1], new Rectangle(270, 160, 483, 383))
+        {
+            Title = "John Smith",
+            Subject = "Circle",
+            Color = Color.Red,
+            InteriorColor = Color.MistyRose,
+            Opacity = 0.5,        
+            Popup = new PopupAnnotation(document.Pages[1], new Rectangle(842, 316, 1021, 459))
+        };
+
+        // Add annotation to the page
+        document.Pages[1].Annotations.Add(circleAnnotation);
+        document.Save(System.IO.Path.Combine(dataDir, "appartments_mod.pdf"));
     }
 }
