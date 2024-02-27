5ce41caba673a4d950be1883fbab2a462bde4c69
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2024-02-27 19:50:21 +0200

    popup

--- Annotator/PopupAnnotator.cs
+++ Annotator/PopupAnnotator.cs
@@ -1,9 +1,31 @@
+using Aspose.Pdf;
+using Aspose.Pdf.Annotations;
+
 namespace aspose.pdf.annotation;
 
 public class PopupAnnotator : IAnnotator
 {
     public void Add()
     {
-        throw new NotImplementedException();
+        // Load the PDF file
+        Document document = new Document(System.IO.Path.Combine("./examples", "Appartments.pdf"));
+
+        // Create Line Annotation
+        var lineAnnotation = new LineAnnotation(
+            document.Pages[1],
+            new Rectangle(550, 93, 562, 439),
+            new Point(556, 99), new Point(556, 443))
+        {
+            Title = "John Smith",
+            Color = Color.Red,
+            Width = 3,
+            StartingStyle = LineEnding.OpenArrow,
+            EndingStyle = LineEnding.OpenArrow,
+            Popup = new PopupAnnotation(document.Pages[1], new Rectangle(842, 124, 1021, 266))
+        };
+
+        // Add annotation to the page
+        document.Pages[1].Annotations.Add(lineAnnotation);
+        document.Save(System.IO.Path.Combine("./examples", "Appartments_mod.pdf"));
     }
 }
