30bb9114b8326a6968b9c2764029d346f6f0be66
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2024-02-27 20:01:01 +0200

    movie

--- Annotator/MovieAnnotator.cs
+++ Annotator/MovieAnnotator.cs
@@ -1,9 +1,23 @@
+using Aspose.Pdf;
+using Aspose.Pdf.Annotations;
+
 namespace aspose.pdf.annotation;
 
 public class MovieAnnotator : IAnnotator
 {
     public void Add()
     {
-        throw new NotImplementedException();
+        // Load the PDF file
+        Document document = new Document(System.IO.Path.Combine("./examples", "sample.pdf"));
+
+        var mediaFile = System.IO.Path.Combine("./examples", "input.swf");
+        // Create Screen Annotation
+        var screenAnnotation = new ScreenAnnotation(
+            document.Pages[1],
+            new Rectangle(170, 190, 470, 380),
+            mediaFile);
+        document.Pages[1].Annotations.Add(screenAnnotation);
+
+        document.Save(System.IO.Path.Combine("./examples", "sample_swf.pdf"));
     }
 }
