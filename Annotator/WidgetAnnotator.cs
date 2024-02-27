76239a7ae0fb93593352db0c717208470f78cf7d
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2024-02-27 20:02:00 +0200

    widget

--- Annotator/WidgetAnnotator.cs
+++ Annotator/WidgetAnnotator.cs
@@ -1,9 +1,33 @@
+using Aspose.Pdf;
+using Aspose.Pdf.Annotations;
+using Aspose.Pdf.Forms;
+
 namespace aspose.pdf.annotation;
 
 public class WidgetAnnotator : IAnnotator
 {
     public void Add()
     {
-        throw new NotImplementedException();
+        var document = new Document();
+        var page = document.Pages.Add();
+        var rect = new Rectangle(72, 748, 164, 768);
+        var printButton = new ButtonField(page, rect)
+        {
+            AlternateName = "Print current document",
+            Color = Color.Black,
+            PartialName = "printBtn1",
+            NormalCaption = "Print Document"
+        };
+        var border = new Border(printButton)
+        {
+            Style = BorderStyle.Solid,
+            Width = 2
+        };
+        printButton.Border = border;
+        printButton.Characteristics.Border =
+            System.Drawing.Color.FromArgb(255, 0, 0, 255);
+        printButton.Characteristics.Background =
+            System.Drawing.Color.FromArgb(255, 0, 191, 255);
+        document.Form.Add(printButton);
     }
 }
