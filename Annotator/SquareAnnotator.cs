2f977a1436ccdf88d438dd9547acf4e9de8fec18
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2024-02-27 19:09:23 +0200

    square

--- Annotator/SquareAnnotator.cs
+++ Annotator/SquareAnnotator.cs
@@ -1,9 +1,29 @@
+using Aspose.Pdf;
+using Aspose.Pdf.Annotations;
+
 namespace aspose.pdf.annotation;
 
 public class SquareAnnotator : IAnnotator
 {
     public void Add()
     {
-        throw new NotImplementedException();
+        var dataDir = "<path-to-file>";
+        // Load the PDF file
+        Document document = new Document(System.IO.Path.Combine(dataDir, "appartments.pdf"));
+
+        // Create Square Annotation
+        var squareAnnotation = new SquareAnnotation(document.Pages[1], new Rectangle(67, 317, 261, 459))
+        {
+            Title = "John Smith",
+            Subject = "Rectangle",
+            Color = Color.Blue,
+            InteriorColor = Color.BlueViolet,
+            Opacity = 0.25,
+            Popup = new PopupAnnotation(document.Pages[1], new Rectangle(842, 196, 1021, 338))
+        };
+
+        // Add annotation to the page
+        document.Pages[1].Annotations.Add(squareAnnotation);
+        document.Save(System.IO.Path.Combine(dataDir, "appartments_mod.pdf"));
     }
 }
