f6080b2d931958e4fcf0862fc0443bc44cd652f6
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2024-02-27 18:36:00 +0200

    link

--- Annotator/LinkAnnotator.cs
+++ Annotator/LinkAnnotator.cs
@@ -1,9 +1,31 @@
+using Aspose.Pdf;
+using Aspose.Pdf.Annotations;
+using Aspose.Pdf.Text;
+
 namespace aspose.pdf.annotation;
 
 public class LinkAnnotator : IAnnotator
 {
     public void Add()
     {
-        throw new NotImplementedException();
+        // Load the PDF file
+        Document document = new Document(System.IO.Path.Combine("./examples", "SimpleResume.pdf"));
+        // Create TextFragmentAbsorber object to find a phone number
+        TextFragmentAbsorber textFragmentAbsorber = new TextFragmentAbsorber("678-555-0103");
+
+        // Accept the absorber for the 1st page only
+        document.Pages[1].Accept(textFragmentAbsorber);
+
+        var phoneNumberFragment = textFragmentAbsorber.TextFragments[1];
+
+        // Create Link Annotation and set the action to call a phone number
+        var linkAnnotation = new LinkAnnotation(document.Pages[1], phoneNumberFragment.Rectangle)
+        {
+            Action = new GoToURIAction("callto:678-555-0103")
+        };
+
+        // Add annotation to page
+        document.Pages[1].Annotations.Add(linkAnnotation);
+        document.Save(System.IO.Path.Combine("./examples", "SimpleResume_mod.pdf"));
     }
 }
