9c7428dfc88ee3db90c81c041b40904aaac4e9e5
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2024-02-28 23:45:33 +0200

    test1

--- Program.cs
+++ Program.cs
@@ -13,64 +13,64 @@ internal class Program
         switch (model)
         {
             case CaretModel caretModel:
-                new CaretAnnotator(caretModel, "./examples", "text.pdf", "caret.pdf").Save();
+                new CaretAnnotator(caretModel, "./examples", "test.pdf", "caret.pdf").Save();
                 break;
             case CircleModel circleModel:
-                new CircleAnnotator(circleModel, "./examples", "text.pdf", "circle.pdf").Save();
+                new CircleAnnotator(circleModel, "./examples", "test.pdf", "circle.pdf").Save();
                 break;
             case FreeTextModel freeTextModel:
-                new FreeTextAnnotator(freeTextModel, "./examples", "text.pdf", "free-text.pdf").Save();
+                new FreeTextAnnotator(freeTextModel, "./examples", "test.pdf", "free-text.pdf").Save();
                 break;
             case HighlightModel highlightModel:
-                new HighlightAnnotator(highlightModel, "./examples", "text.pdf", "highlight.pdf").Save();
+                new HighlightAnnotator(highlightModel, "./examples", "test.pdf", "highlight.pdf").Save();
                 break;
             case InkModel inkModel:
-                new InkAnnotator(inkModel, "./examples", "text.pdf", "ink.pdf").Save();
+                new InkAnnotator(inkModel, "./examples", "test.pdf", "ink.pdf").Save();
                 break;
             case LineModel lineModel:
-                new LineAnnotator(lineModel, "./examples", "text.pdf", "line.pdf").Save();
+                new LineAnnotator(lineModel, "./examples", "test.pdf", "line.pdf").Save();
                 break;
             case LinkModel linkModel:
-                new LinkAnnotator(linkModel, "./examples", "text.pdf", "link.pdf").Save();
+                new LinkAnnotator(linkModel, "./examples", "test.pdf", "link.pdf").Save();
                 break;
             case MovieModel movieModel:
-                new MovieAnnotator(movieModel, "./examples", "text.pdf", "movie.pdf").Save();
+                new MovieAnnotator(movieModel, "./examples", "test.pdf", "movie.pdf").Save();
                 break;
             case PolygonModel polygonModel:
-                new PolygonAnnotator(polygonModel, "./examples", "text.pdf", "polygon.pdf").Save();
+                new PolygonAnnotator(polygonModel, "./examples", "test.pdf", "polygon.pdf").Save();
                 break;
             case PolyLineModel polyLineModel:
-                new PolyLineAnnotator(polyLineModel, "./examples", "text.pdf", "polyLine.pdf").Save();
+                new PolyLineAnnotator(polyLineModel, "./examples", "test.pdf", "polyLine.pdf").Save();
                 break;
             case RedactModel redactModel:
-                new RedactAnnotator(redactModel, "./examples", "text.pdf", "redact.pdf").Save();
+                new RedactAnnotator(redactModel, "./examples", "test.pdf", "redact.pdf").Save();
                 break;
             case ScreenModel screenModel:
-                new ScreenAnnotator(screenModel, "./examples", "text.pdf", "screen.pdf").Save();
+                new ScreenAnnotator(screenModel, "./examples", "test.pdf", "screen.pdf").Save();
                 break;
             case SoundModel soundModel:
-                new SoundAnnotator(soundModel, "./examples", "text.pdf", "sound.pdf").Save();
+                new SoundAnnotator(soundModel, "./examples", "test.pdf", "sound.pdf").Save();
                 break;
             case SquareModel squareModel:
-                new SquareAnnotator(squareModel, "./examples", "text.pdf", "square.pdf").Save();
+                new SquareAnnotator(squareModel, "./examples", "test.pdf", "square.pdf").Save();
                 break;
             case SquigglyModel squigglyModel:
-                new SquigglyAnnotator(squigglyModel, "./examples", "text.pdf", "squiggly.pdf").Save();
+                new SquigglyAnnotator(squigglyModel, "./examples", "test.pdf", "squiggly.pdf").Save();
                 break;
             case StampModel stampModel:
-                new StampAnnotator(stampModel, "./examples", "text.pdf", "stamp.pdf").Save();
+                new StampAnnotator(stampModel, "./examples", "test.pdf", "stamp.pdf").Save();
                 break;
             case StrikeOutModel strikeOutModel:
-                new StrikeOutAnnotator(strikeOutModel, "./examples", "text.pdf", "strike-out.pdf").Save();
+                new StrikeOutAnnotator(strikeOutModel, "./examples", "test.pdf", "strike-out.pdf").Save();
                 break;
             case TextModel textModel:
-                new TextAnnotator(textModel, "./examples", "text.pdf", "text.pdf").Save();
+                new TextAnnotator(textModel, "./examples", "test.pdf", "text.pdf").Save();
                 break;
             case UnderlineModel underlineModel:
-                new UnderlineAnnotator(underlineModel, "./examples", "text.pdf", "underline.pdf").Save();
+                new UnderlineAnnotator(underlineModel, "./examples", "test.pdf", "underline.pdf").Save();
                 break;
             case WatermarkModel watermarkModel:
-                new WatermarkAnnotator(watermarkModel, "./examples", "text.pdf", "watermark.pdf").Save();
+                new WatermarkAnnotator(watermarkModel, "./examples", "test.pdf", "watermark.pdf").Save();
                 break;
             default:
                 Console.WriteLine("unknown:"+ model.GetType());
--- Tests.cs
+++ Tests.cs
@@ -6,26 +6,26 @@ namespace aspose.pdf.annotation
     {
         public void Run()
         {
-            new CaretAnnotator(new CaretModel(), "./examples", "text.pdf", "caret.pdf").Save();
-            new CircleAnnotator(new CircleModel(), "./examples", "text.pdf", "circle.pdf").Save();
-            new FreeTextAnnotator(new FreeTextModel(), "./examples", "text.pdf", "free-text.pdf").Save();
-            new HighlightAnnotator(new HighlightModel(), "./examples", "text.pdf", "highlight.pdf").Save();
-            new InkAnnotator(new InkModel(), "./examples", "text.pdf", "ink.pdf").Save();
-            new LineAnnotator(new LineModel(), "./examples", "text.pdf", "line.pdf").Save();
-            new LinkAnnotator(new LinkModel(), "./examples", "text.pdf", "link.pdf").Save();
-            new MovieAnnotator(new MovieModel(), "./examples", "text.pdf", "movie.pdf").Save();
-            new PolygonAnnotator(new PolygonModel(), "./examples", "text.pdf", "polygon.pdf").Save();
-            new PolyLineAnnotator(new PolyLineModel(), "./examples", "text.pdf", "polyLine.pdf").Save();
-            new RedactAnnotator(new RedactModel(), "./examples", "text.pdf", "redact.pdf").Save();
-            new ScreenAnnotator(new ScreenModel(), "./examples", "text.pdf", "screen.pdf").Save();
-            new SoundAnnotator(new SoundModel(), "./examples", "text.pdf", "sound.pdf").Save();
-            new SquareAnnotator(new SquareModel(), "./examples", "text.pdf", "square.pdf").Save();
-            new SquigglyAnnotator(new SquigglyModel(), "./examples", "text.pdf", "squiggly.pdf").Save();
-            new StampAnnotator(new StampModel(), "./examples", "text.pdf", "stamp.pdf").Save();
-            new StrikeOutAnnotator(new StrikeOutModel(), "./examples", "text.pdf", "strike-out.pdf").Save();
-            new TextAnnotator(new TextModel(), "./examples", "text.pdf", "text.pdf").Save();
-            new UnderlineAnnotator(new UnderlineModel(), "./examples", "text.pdf", "underline.pdf").Save();
-            new WatermarkAnnotator(new WatermarkModel(), "./examples", "text.pdf", "watermark.pdf").Save();
+            new CaretAnnotator(new CaretModel(), "./examples", "test.pdf", "caret.pdf").Save();
+            new CircleAnnotator(new CircleModel(), "./examples", "test.pdf", "circle.pdf").Save();
+            new FreeTextAnnotator(new FreeTextModel(), "./examples", "test.pdf", "free-text.pdf").Save();
+            new HighlightAnnotator(new HighlightModel(), "./examples", "test.pdf", "highlight.pdf").Save();
+            new InkAnnotator(new InkModel(), "./examples", "test.pdf", "ink.pdf").Save();
+            new LineAnnotator(new LineModel(), "./examples", "test.pdf", "line.pdf").Save();
+            new LinkAnnotator(new LinkModel(), "./examples", "test.pdf", "link.pdf").Save();
+            new MovieAnnotator(new MovieModel(), "./examples", "test.pdf", "movie.pdf").Save();
+            new PolygonAnnotator(new PolygonModel(), "./examples", "test.pdf", "polygon.pdf").Save();
+            new PolyLineAnnotator(new PolyLineModel(), "./examples", "test.pdf", "polyLine.pdf").Save();
+            new RedactAnnotator(new RedactModel(), "./examples", "test.pdf", "redact.pdf").Save();
+            new ScreenAnnotator(new ScreenModel(), "./examples", "test.pdf", "screen.pdf").Save();
+            new SoundAnnotator(new SoundModel(), "./examples", "test.pdf", "sound.pdf").Save();
+            new SquareAnnotator(new SquareModel(), "./examples", "test.pdf", "square.pdf").Save();
+            new SquigglyAnnotator(new SquigglyModel(), "./examples", "test.pdf", "squiggly.pdf").Save();
+            new StampAnnotator(new StampModel(), "./examples", "test.pdf", "stamp.pdf").Save();
+            new StrikeOutAnnotator(new StrikeOutModel(), "./examples", "test.pdf", "strike-out.pdf").Save();
+            new TextAnnotator(new TextModel(), "./examples", "test.pdf", "text.pdf").Save();
+            new UnderlineAnnotator(new UnderlineModel(), "./examples", "test.pdf", "underline.pdf").Save();
+            new WatermarkAnnotator(new WatermarkModel(), "./examples", "test.pdf", "watermark.pdf").Save();
         }
     }
 }
